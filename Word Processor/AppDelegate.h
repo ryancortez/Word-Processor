6694b82096dfa482b54491110eba74262c2da93c
Ryan Cortez <ryancortez@me.com>
2016-06-16 14:38:28 -0500

    Initial Commit

---
+++ Word Processor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Word Processor
+//
+//  Created by Ryan on 6/16/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Word Processor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Word Processor
+//
+//  Created by Ryan on 6/16/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
